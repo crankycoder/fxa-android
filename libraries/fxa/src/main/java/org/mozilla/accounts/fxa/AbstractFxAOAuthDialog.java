04980f2d091a07778dc3c9a9fb2a40c581e3f27d
Victor Ng <victor@crankycoder.com>
2015-06-02 18:38:41 -0400

    * Dropped custom titles * Dropped useless error handler and the entire OAuthWebViewClient * Removed orientation code and just make the login full screen all the time

--- libraries/fxa/src/main/java/org/mozilla/accounts/fxa/AbstractFxAOAuthDialog.java
+++ libraries/fxa/src/main/java/org/mozilla/accounts/fxa/AbstractFxAOAuthDialog.java
@@ -1,54 +1,33 @@
 package org.mozilla.accounts.fxa;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public abstract class AbstractFxAOAuthDialog extends Dialog {
     private static final String LOG_TAG = LoggerUtil.makeLogTag(AbstractFxAOAuthDialog.class);
-
-    static final int BG_COLOR = Color.LTGRAY;//0xFF6D84B4;
-    static final float[] DIMENSIONS_LANDSCAPE = {460, 260};
-    static final float[] DIMENSIONS_PORTRAIT = {280, 420};
-    static final FrameLayout.LayoutParams FILL =
-        new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
-                         ViewGroup.LayoutParams.FILL_PARENT);
-    static final int MARGIN = 4;
-    static final int PADDING = 2;
-
     private final String mAppCallback;
-
     private String mUrl;
 
-
-    private ProgressDialog mSpinner;
     private WebView mWebView;
     private LinearLayout mContent;
-    private TextView mTitle;
 
     public AbstractFxAOAuthDialog(Context context,
                                   String signinUrl,
                                   String appCallback,
                                   String[] scopes,
                                   String AppKey) {
-        super(context);
+        super(context, android.R.style.Theme_Light);
+        // Wipe down all the DOM storage
+        WebStorage.getInstance().deleteAllData();
+
         mAppCallback = appCallback;
 
         String scopeText = TextUtils.join(" ", scopes);
@@ -62,45 +41,17 @@ public abstract class AbstractFxAOAuthDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSpinner = new ProgressDialog(getContext());
-        mSpinner.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mSpinner.setMessage("Loading...");
-
         mContent = new LinearLayout(getContext());
         mContent.setOrientation(LinearLayout.VERTICAL);
-        setUpTitle();
+        setTitle("Firefox Accounts");
         setUpWebView();
-        final float scale = getContext().getResources().getDisplayMetrics().density;
-
-        int orientation = getContext().getResources().getConfiguration().orientation;
-
-        float[] dimensions;
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            dimensions = DIMENSIONS_LANDSCAPE;
-        } else {
-            dimensions = DIMENSIONS_PORTRAIT;
-        }
-        addContentView(mContent, new FrameLayout.LayoutParams(
-        		(int) (dimensions[0] * scale + 0.5f),
-        		(int) (dimensions[1] * scale + 0.5f)));
-    }
-
-    private void setUpTitle() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mTitle = new TextView(getContext());
-        mTitle.setText("Firefox Accounts");
-        mTitle.setTextColor(Color.WHITE);
-        mTitle.setTypeface(Typeface.DEFAULT_BOLD);
-        mTitle.setBackgroundColor(BG_COLOR);
-        mTitle.setPadding(MARGIN + PADDING, MARGIN, MARGIN, MARGIN);
-        mContent.addView(mTitle);
+        setContentView(mContent);
     }
 
     private void setUpWebView() {
         mWebView = new WebView(getContext());
         mWebView.setVerticalScrollBarEnabled(false);
         mWebView.setHorizontalScrollBarEnabled(false);
-        mWebView.setWebViewClient(new AbstractFxAOAuthDialog.OAuthWebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.addJavascriptInterface(new ContentViewer(this), "HTMLOUT");
         mWebView.getSettings().setDomStorageEnabled(true);
@@ -113,10 +64,7 @@ public abstract class AbstractFxAOAuthDialog extends Dialog {
                 mWebView.loadUrl("javascript:window.HTMLOUT.showHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
             }
         });
-
-        Log.i(LOG_TAG, "mUrl = ["+mUrl+"]");
         mWebView.loadUrl(mUrl);
-        mWebView.setLayoutParams(FILL);
         mContent.addView(mWebView);
     }
 
@@ -136,47 +84,11 @@ public abstract class AbstractFxAOAuthDialog extends Dialog {
             LocalBroadcastManager
                     .getInstance(getContext())
                     .sendBroadcast(fxaOauthIntent);
-
-            // Wipe down all the DOM storage
-            WebStorage.getInstance().deleteAllData();
         }
+        // Wipe down all the DOM storage
+        WebStorage.getInstance().deleteAllData();
     }
 
-    private class OAuthWebViewClient extends WebViewClient {
-    
-        @Override
-        public void onReceivedError(WebView view, int errorCode,
-                String description, String failingUrl) {
-            super.onReceivedError(view, errorCode, description, failingUrl);
-            AbstractFxAOAuthDialog.this.dismiss();
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            Log.d(LOG_TAG, "onPageStarted->Webview loading URL: " + url);
-            super.onPageStarted(view, url, favicon);
-            mSpinner.show();
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-        	Log.d(LOG_TAG, "onPageFinished->Webview URL: " + url);
-        	super.onPageFinished(view, url);
-        	
-            String title = mWebView.getTitle();
-            if (title != null && title.length() > 0) {
-                mTitle.setText(title);
-            }
-          
-            try {
-	            if (mSpinner!=null && mSpinner.isShowing())
-	            	mSpinner.dismiss();
-            }
-            catch (Exception ex) {
-            	Log.w(LOG_TAG, "wtf exception onPageFinished! " + ex.toString());
-            }
-        }
-    }
 
     private class ContentViewer {
 
